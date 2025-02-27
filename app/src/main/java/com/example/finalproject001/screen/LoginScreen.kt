9e9c7194e3c00e50e9d570eae76bf6a20b2705ea
4Diyusu <kevin.enerio@outlook.com>
2025-02-27 17:46:15 +0800

    Added back signup label for LoginScreen.kt

--- app/src/main/java/com/example/finalproject001/screen/LoginScreen.kt
+++ app/src/main/java/com/example/finalproject001/screen/LoginScreen.kt
@@ -8,10 +8,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -21,6 +25,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -109,8 +114,27 @@ fun LoginScreen(modifier: Modifier = Modifier, navController: NavController, aut
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Text(text = "Forgot Password?", modifier = Modifier.clickable{
+        Row (
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.Center
+        ){
+            Text(text = "Forgot Password?", color = Color.Magenta,
+                modifier = Modifier.clickable{
 
-        })
+            })
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Text(text = "or")
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Text(text = "Signup", color = Color.Blue,
+                modifier = Modifier.clickable{
+                    navController.navigate(Routes.registrationScreen) {
+                        popUpTo(Routes.loginScreen) { inclusive = true }
+                    }
+                })
+        }
     }
 }
